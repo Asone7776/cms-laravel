842d75f8de2d4fece910443b6fb552c16a9f1c68
aobichkin@uzkassa.uz <aA123456>
2021-06-11 00:56:10 -0700

    Initial commit

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('content')
+    <div class="card" style="width: 18rem;">
+        <div class="card-header">
+            Категории
+        </div>
+        <div class="card-body">
+            Categories content
+        </div>
+    </div>
+@stop
